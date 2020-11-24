24dce87339aaa0349862c246684093a0a836621b
kharlamov_dk <harlamov_da@smart-transport.ru>
2020-11-24 14:36:25 +0500

    modal windows added

--- src/components/HospitalsPage/ModalButtons/ModalButtons.js
+++ src/components/HospitalsPage/ModalButtons/ModalButtons.js
@@ -1,13 +1,44 @@
 import React from "react";
 import { InfoCircleOutlined } from "@ant-design/icons";
 import { Button } from "antd";
+import { Modal } from 'antd';
 
 const PatientsButton = (props) => {
+  const [visible, setVisible] = React.useState(false);
+  const [confirmLoading, setConfirmLoading] = React.useState(false);
+  const [modalText, setModalText] = React.useState('Content of the modal');
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setModalText('The modal will be closed after two seconds');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setVisible(false);
+  };
   return (
     <div className={"patients_button"}>
-      <Button size={"large"}>
+      <Button size={"large"} onClick={showModal}>
         {props.title}
       </Button>
+      <Modal
+          title="Title"
+          visible={visible}
+          onOk={handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={handleCancel}
+        >
+          <p>{modalText}</p>
+        </Modal>
     </div>
   );
 };

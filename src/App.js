f3dcc074aabb702c4b0e1a7e92a63afbb9100864
kharlamov_dk <harlamov_da@smart-transport.ru>
2020-11-25 12:20:14 +0500

    counter added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import {BrowserRouter, Redirect, Route} from 'react-router-dom'
 import AuthorizationPage from './components/Authorization/AuthorizationPage';
 import Hospitals from './components/HospitalsPage/Hospitals';
-import { getDepartmentsThunk, getSingleDepartmentThunk } from './context/reducers/departmentsReducer';
 import './index.css'
 
 function App() {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(getDepartmentsThunk())
-  }, [dispatch])
-
-  const deps = useSelector(state => state.departments.departments)
-  console.log(deps.data)
   const isAuth = localStorage.access
   return (
     <BrowserRouter className="App">
--- src/components/HospitalsPage/PlacesInfo/PlacesInfo.js
+++ src/components/HospitalsPage/PlacesInfo/PlacesInfo.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import '../Content.css'
 
 export const PlacesInfo = () => {
+  const placesCount = useSelector(state => state.departments.department)
+  const free = placesCount?.data?.count_female_free +
+  placesCount?.data?.count_female_o2_free + 
+  placesCount?.data?.count_male_free +
+  placesCount?.data?.count_male_o2_free
   return (
     <div>
       <h1>Места в стационаре</h1>
       <div className={"hospital_places"}>
         <p>
-          Свободно <span>60</span> Занято <span>60</span> Всего <span>60</span>
+          Свободно <span>{free || 0}</span> Занято <span>60</span> Всего <span>60</span>
         </p>
       </div>
     </div>

87db75ddbc85d3219bbe41ab1851c85193191a85
kharlamov_dk <harlamov_da@smart-transport.ru>
2020-11-25 12:23:10 +0500

    counter added

--- src/components/HospitalsPage/PlacesInfo/PlacesInfo.js
+++ src/components/HospitalsPage/PlacesInfo/PlacesInfo.js
@@ -8,12 +8,20 @@ export const PlacesInfo = () => {
   placesCount?.data?.count_female_o2_free + 
   placesCount?.data?.count_male_free +
   placesCount?.data?.count_male_o2_free
+
+  const busy = placesCount?.data?.count_female_busy +
+  placesCount?.data?.count_female_o2_busy + 
+  placesCount?.data?.count_male_busy +
+  placesCount?.data?.count_male_o2_busy
+
+  const total = busy + free
+
   return (
     <div>
       <h1>Места в стационаре</h1>
       <div className={"hospital_places"}>
         <p>
-          Свободно <span>{free || 0}</span> Занято <span>60</span> Всего <span>60</span>
+          Свободно <span>{free || 0}</span> Занято <span>{busy || 0}</span> Всего <span>{total || 0}</span>
         </p>
       </div>
     </div>
